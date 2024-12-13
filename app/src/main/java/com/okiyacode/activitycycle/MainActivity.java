7641f02da429c0cd33307cc16fba0cc5985e4faf
= <=>
2024-12-13 11:43:19 +0100

    add all android life cycle function mean implement

--- app/src/main/java/com/okiyacode/activitycycle/MainActivity.java
+++ app/src/main/java/com/okiyacode/activitycycle/MainActivity.java
@@ -1,6 +1,10 @@
 package com.okiyacode.activitycycle;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,16 +14,82 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    ImageView btnInfo;
+    Context context;
+    private void setup() {
+        context = this;
+        btnInfo = findViewById(R.id.btnInfoID);
+    }
+
+    private void display(String msg) {
+        Toast.makeText(context,msg, Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        setup();
+
+        display("onCreate");
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+
+
+        btnInfo.setOnClickListener(View-> {
+
+            Intent intent = new Intent(MainActivity.this, how_work.class);
+            startActivity(intent);
+
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        display("onStart");
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        display("onResume");
+
+    }
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        display("onRestart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        display("onPause");
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        display("onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        display("onDestroy");
     }
 }
---
+++ app/src/main/java/com/okiyacode/activitycycle/how_work.java
@@ -0,0 +1,29 @@
+package com.okiyacode.activitycycle;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class how_work extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_how_work);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+    }
+}
